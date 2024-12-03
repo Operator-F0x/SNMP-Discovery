13657bd9d68a442db3849e2a2d8f0144ff824e99
KaSaNaa <pasindudeshitha123@gmail.com>
2024-12-03 17:17:51 +0530

    Add SNMP neighbor discovery and save results to neighbors.json

--- network_discovery/main.py
+++ network_discovery/main.py
@@ -1,16 +1,26 @@
 from utils.network import get_ips_from_subnets, scan_subnet
-from utils.graph import build_topology, draw_topology
+from utils.snmp import get_snmp_neighbors
+import json
 
 # Example usage
 if __name__ == "__main__":
     subnet = ["192.168.62.0/24"]
     all_ips = get_ips_from_subnets(subnet)
-    # print(all_ips)
 
     
     active_ips = scan_subnet(all_ips)
     print("\n\nActive IPs")
     for ip in active_ips:
         print(ip)
-    # topology = build_topology(active_ips, user, auth_key, priv_key, auth_protocol, priv_protocol)
-    # draw_topology(topology)
+    
+    all_neighbors = {}
+    
+    for ip in active_ips:
+        neighbors = get_snmp_neighbors(ip, 2, community="public")
+        all_neighbors[ip] = neighbors
+        print("\n\nNeighbors for", ip)
+        print(neighbors)
+    
+    # Save the neighbors data to a JSON file
+    with open('neighbors.json', 'w') as json_file:
+        json.dump(all_neighbors, json_file, indent=4)
